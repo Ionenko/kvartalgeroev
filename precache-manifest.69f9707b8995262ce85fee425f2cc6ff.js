0d028ab9d35483cb90b7cad75839dbff58aaa50c
Ionenko Vladimir <ionenkovladimir@gmail.com>
2019-12-19 16:04:54 +0200

    Updates

---
+++ precache-manifest.69f9707b8995262ce85fee425f2cc6ff.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffe56105bc0f5479d3ba3a72215da019",
+    "url": "/kvartalgeroev/index.html"
+  },
+  {
+    "revision": "331c19650f9e1c24c5b7",
+    "url": "/kvartalgeroev/static/css/main.488cb3b4.chunk.css"
+  },
+  {
+    "revision": "7836916409727646a65d",
+    "url": "/kvartalgeroev/static/js/2.c2c36ead.chunk.js"
+  },
+  {
+    "revision": "331c19650f9e1c24c5b7",
+    "url": "/kvartalgeroev/static/js/main.52a4d5ec.chunk.js"
+  },
+  {
+    "revision": "4cef6fdf2f8077c31e35",
+    "url": "/kvartalgeroev/static/js/runtime-main.e273c7df.js"
+  },
+  {
+    "revision": "655337ff4641f76ac6a814c1244ccf56",
+    "url": "/kvartalgeroev/static/media/1.655337ff.svg"
+  },
+  {
+    "revision": "4ec682e74cff88aee2e16215b13e955f",
+    "url": "/kvartalgeroev/static/media/11.4ec682e7.svg"
+  },
+  {
+    "revision": "777ee5e066dd7bc857b8a3647d9bacfd",
+    "url": "/kvartalgeroev/static/media/2.777ee5e0.svg"
+  },
+  {
+    "revision": "13209d26bca5a0d0944cc42f8060d497",
+    "url": "/kvartalgeroev/static/media/3.13209d26.svg"
+  },
+  {
+    "revision": "d040aded125f3d52ce572d1458374fd5",
+    "url": "/kvartalgeroev/static/media/4.d040aded.svg"
+  },
+  {
+    "revision": "70578c9ad79670598484adbe49d100dc",
+    "url": "/kvartalgeroev/static/media/404.70578c9a.png"
+  },
+  {
+    "revision": "4c1e64e740e61f7b1cea5556abce04c9",
+    "url": "/kvartalgeroev/static/media/5.4c1e64e7.svg"
+  },
+  {
+    "revision": "c2538bbe35242568504dfc1886162402",
+    "url": "/kvartalgeroev/static/media/6.c2538bbe.svg"
+  },
+  {
+    "revision": "c3fd8c447702b2de238e25adada98761",
+    "url": "/kvartalgeroev/static/media/7.c3fd8c44.svg"
+  },
+  {
+    "revision": "a4cd6c8930d192d7f760a0d064396ad7",
+    "url": "/kvartalgeroev/static/media/8.a4cd6c89.svg"
+  },
+  {
+    "revision": "32353ede16b2759796f4280bb9a04a4e",
+    "url": "/kvartalgeroev/static/media/9.32353ede.svg"
+  },
+  {
+    "revision": "312d74dd7177994096623b61baf4c323",
+    "url": "/kvartalgeroev/static/media/Acrom-Bold.312d74dd.woff"
+  },
+  {
+    "revision": "3dc8b6da1c955f9d463165ecbfad9fbe",
+    "url": "/kvartalgeroev/static/media/Acrom-Bold.3dc8b6da.ttf"
+  },
+  {
+    "revision": "a9f868b2ac2c429afa5dc8f31a256c44",
+    "url": "/kvartalgeroev/static/media/Acrom-Bold.a9f868b2.eot"
+  },
+  {
+    "revision": "8682a4ca7b6648d6b4a7aece02dbaaa4",
+    "url": "/kvartalgeroev/static/media/Acrom-ExtraBold.8682a4ca.woff"
+  },
+  {
+    "revision": "86de4ebb581c246d1b899c85c0bd7939",
+    "url": "/kvartalgeroev/static/media/Acrom-ExtraBold.86de4ebb.eot"
+  },
+  {
+    "revision": "fe723a3df2bb4798c24caac79f47d189",
+    "url": "/kvartalgeroev/static/media/Acrom-ExtraBold.fe723a3d.ttf"
+  },
+  {
+    "revision": "1f22412f04f3170eac2d8df315d37683",
+    "url": "/kvartalgeroev/static/media/Acrom-Light.1f22412f.ttf"
+  },
+  {
+    "revision": "c932b3f8c382a8160c95a0bd0c080ee2",
+    "url": "/kvartalgeroev/static/media/Acrom-Light.c932b3f8.woff"
+  },
+  {
+    "revision": "cdebf4e2678f83dd50be60f7844c704d",
+    "url": "/kvartalgeroev/static/media/Acrom-Light.cdebf4e2.eot"
+  },
+  {
+    "revision": "a4ea8642da8181082ff78a069688f913",
+    "url": "/kvartalgeroev/static/media/Acrom-Medium.a4ea8642.eot"
+  },
+  {
+    "revision": "ddf231c1e4021ce2dcb3224cf389a14e",
+    "url": "/kvartalgeroev/static/media/Acrom-Medium.ddf231c1.woff"
+  },
+  {
+    "revision": "eff1aba8aebe5113783baf68647c1dc6",
+    "url": "/kvartalgeroev/static/media/Acrom-Medium.eff1aba8.ttf"
+  },
+  {
+    "revision": "538f07188b6a0d95eba8af288944f357",
+    "url": "/kvartalgeroev/static/media/Acrom-Regular.538f0718.ttf"
+  },
+  {
+    "revision": "7dad626af555965db61439c811deae99",
+    "url": "/kvartalgeroev/static/media/Acrom-Regular.7dad626a.woff"
+  },
+  {
+    "revision": "a7697576e7e7e6278220ab052768701c",
+    "url": "/kvartalgeroev/static/media/Acrom-Regular.a7697576.eot"
+  },
+  {
+    "revision": "5d68c342edb6dd7de1cf46065f79e775",
+    "url": "/kvartalgeroev/static/media/Acrom-Thin.5d68c342.ttf"
+  },
+  {
+    "revision": "74f6c69f76fe94e07eaaa098643cba2a",
+    "url": "/kvartalgeroev/static/media/Acrom-Thin.74f6c69f.woff"
+  },
+  {
+    "revision": "fa6348bc6c434f0b372d4c658c60850c",
+    "url": "/kvartalgeroev/static/media/Acrom-Thin.fa6348bc.eot"
+  },
+  {
+    "revision": "5f9f77ea477720306de966707fabad25",
+    "url": "/kvartalgeroev/static/media/download.5f9f77ea.svg"
+  },
+  {
+    "revision": "01c5b4c7725711af40c307259e3a84b7",
+    "url": "/kvartalgeroev/static/media/flat.01c5b4c7.svg"
+  },
+  {
+    "revision": "35b1ddb7697044c8617c908c6a3ae834",
+    "url": "/kvartalgeroev/static/media/icomoon.35b1ddb7.svg"
+  },
+  {
+    "revision": "bf8b7e691a1a1a4379c17de2229f8c5e",
+    "url": "/kvartalgeroev/static/media/icomoon.bf8b7e69.eot"
+  },
+  {
+    "revision": "ce7825a0f386316321ed45c66aea70f3",
+    "url": "/kvartalgeroev/static/media/icomoon.ce7825a0.woff"
+  },
+  {
+    "revision": "f101f61933a4c8e05ffc7b64d1bad929",
+    "url": "/kvartalgeroev/static/media/icomoon.f101f619.ttf"
+  },
+  {
+    "revision": "d31353997ae31aa0a4700b1b9da11767",
+    "url": "/kvartalgeroev/static/media/logo.d3135399.svg"
+  },
+  {
+    "revision": "2c53a53fd12c5ca283005201e600e275",
+    "url": "/kvartalgeroev/static/media/logoSlide.2c53a53f.svg"
+  },
+  {
+    "revision": "7db0d5cc9a8cf4d2075fd5712ecdc4bd",
+    "url": "/kvartalgeroev/static/media/panorama.7db0d5cc.svg"
+  },
+  {
+    "revision": "495f1b49c00d11e6aaa370ee877a98cf",
+    "url": "/kvartalgeroev/static/media/phone.495f1b49.svg"
+  }
+]);
